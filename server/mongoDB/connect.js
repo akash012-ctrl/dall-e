a28002549af6cb3c8de23dc414ffc455f86291a5
akash012-ctrl <akashgo.ac@gmail.com>
2023-01-31 19:59:12 +0530

    changes in mongoDB connection

--- server/mongoDB/connect.js
+++ server/mongoDB/connect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async (url) =>{
-    mongoose.set('strictQuery',true);
+    
     mongoose.connect(url)
     .then(()=>{
         console.log("MonogoDB connected");
