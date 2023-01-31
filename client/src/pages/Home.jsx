da808150384b4dbc06c536111a0d0d680329ae46
akash012-ctrl <akashgo.ac@gmail.com>
2023-01-31 20:15:38 +0530

    adding hosted url of the backend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,12 +24,15 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:3000/api/v1/post", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await fetch(
+          "https://dall-e-x6bl.onrender.com/api/v1/post",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
         if (response.ok) {
           const result = await response.json();
           setAllPosts(result.data.reverse());
